646239886593cafbcb80b94bbe9e946f18b0ee13
VAIBHAV-VOLT <vaibhavagra2005@gmail.com>
2026-03-20 22:43:12 +0530

    backend first

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-// Express Server Entry
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("NeuroPlay Backend Running 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
