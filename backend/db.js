ce93a79b09a9baf045dd3867419d7969f2a266ea
rahulbuildsstuff <rahulnalla.tech@gmail.com>
2026-03-26 11:40:05 +0530

    Error resolved in db.js

--- backend/db.js
+++ backend/db.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
-    console.log('DB connection error:', err);
+    console.error('DB connection error:', err);
   } else {
-    console.log('Connected to MySQL');
+    console.log('Connected to MySQL via connection pool');
+    connection.release();
   }
 });
 
